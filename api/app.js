923222b423573af9fb63b3566a8165cfc9ddb5b7
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2021-11-21 19:22:39 -0500

    Add more env variables

--- api/app.js
+++ api/app.js
@@ -11,13 +11,14 @@ const usersRouter = require('./routes/users');
 const app = express();
 
 // Setting up Mongoose and MongoDB
-const mongoose = require('mongoose');                                                                                         //Import the mongoose module
+const mongoose = require('mongoose');                                                                                             //Import the mongoose module
+const dbUserName = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DBNAME;
-const mongoDB = `mongodb+srv://eric_main:${dbPassword}@cluster0.zwekp.mongodb.net/${dbName}?retryWrites=true&w=majority`;     // Store the DB origin
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});                                                 //Set up default mongoose connection
-const db = mongoose.connection;                                                                                               //Get the default connection
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));                                                     //Bind connection to error event (to get notification of connection errors)
+const mongoDB = `mongodb+srv://${dbUserName}:${dbPassword}@cluster0.zwekp.mongodb.net/${dbName}?retryWrites=true&w=majority`;     // Store the DB origin
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});                                                     //Set up default mongoose connection
+const db = mongoose.connection;                                                                                                   //Get the default connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));                                                         //Bind connection to error event (to get notification of connection errors)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));

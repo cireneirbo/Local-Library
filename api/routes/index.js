7c5599e61ebe040a524f966d8a84656fd70c201a
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2021-11-23 16:26:11 -0500

    Add redirect to site.com/catalog/

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/catalog/'); // redirects the home page to catalog index
 });
 
 module.exports = router;

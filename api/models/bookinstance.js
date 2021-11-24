6f4db617e8d495bab2f1dc7b1ebc622aed4b8943
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2021-11-24 11:52:23 -0500

    Add Luxon library and prettify the due dates

--- api/models/bookinstance.js
+++ api/models/bookinstance.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const Schema = mongoose.Schema;
 
@@ -18,6 +19,13 @@ BookInstanceSchema
         return '/catalog/bookinstance/' + this._id;
     }
 );
+
+// Virtual for bookinstance's DateTime
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
+});
 
 //Export model
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
